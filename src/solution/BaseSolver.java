ecef38898dbd45b75c7aaedb89f6f503bdc847ce
Guille <guillegrear@gmail.com>
2026-05-13 17:25:01 +0200

    Mejora heurística: FFD con bucles for

--- src/solution/BaseSolver.java
+++ src/solution/BaseSolver.java
@@ -4,13 +4,43 @@ import core.BinPackingSolver;
 import java.util.*;
 
 public class BaseSolver implements BinPackingSolver {
-    @Override
-    public List<List<Double>> solve(double capacity, List<Double> weights) {
-        List<List<Double>> bins = new ArrayList<>();
+	@Override
+	public List<List<Double>> solve(double capacity, List<Double> weights) {
+		
+		/*
+		List<List<Double>> bins = new ArrayList<>();
+		
+		for (double weight: weights) {
+			bins.add(List.of(weight));
+		}
+		return bins;
+		*/
+		
+		
+		List<List<Double>> bins = new ArrayList<>();
+		List<Double> sortedWeights = weights;
+		
+		List<Double> contenedor = new ArrayList<>();
+		Double currentCapacity = capacity;
 
-        for (double weight: weights) {
-        	bins.add(List.of(weight));
-        }
-        return bins;
-    }
+		while (!sortedWeights.isEmpty()) {
+			contenedor = new ArrayList<>();
+			currentCapacity = capacity;
+			
+			for (int w = 0; w < sortedWeights.size(); w++) {
+				if (sortedWeights.get(w) <= currentCapacity) {
+					contenedor.add(sortedWeights.get(w));
+					currentCapacity -= sortedWeights.get(w);
+					sortedWeights.remove(sortedWeights.get(w));
+				} else {
+					
+				}
+			}
+			
+			bins.add(contenedor);
+		}
+		
+		return bins;
+		
+	}
 }
